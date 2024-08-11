239655bc6b1758da8aee8fb1d2f596f7179ac0e6
Sami <samiberakih3@gmail.com>
2024-08-11 15:20:04 +0300

    add a new file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,7 +6,9 @@ import WelcomeMessage from './WelcomeMessage'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from './UserProfile';
 
+<UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
 
 function App() {
   return (

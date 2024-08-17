01054f601427433d4ebc70be8741f1684c224777
Sami <samiberakih3@gmail.com>
2024-08-17 18:57:54 +0300

    updated new react app

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,4 +1,16 @@
 function Footer() {
+
+    const footerStyle = {
+        backgroundColor: '#282c34',
+        color: 'white',
+        padding: '10px',
+        textAlign: 'center',
+        position: 'fixed',
+        bottom: '0',
+        width: '100%',
+    };
+
+
     return (
         <footer>
             <p>© 2023 City Lovers</p>
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,4 +1,12 @@
 function MainContent() {
+
+        const mainContentStyle = {
+            padding: '20px',
+            backgroundColor: '#f4f4f4',
+            color: '#333',
+        };
+    
+
     return (
         <main>
             <p>I love to visit New York, Paris, and Tokyo.</p>

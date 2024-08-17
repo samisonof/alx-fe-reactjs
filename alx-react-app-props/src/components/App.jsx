45d69b7d6f95ce9213496bbbfef5dcaa1cb8fc4e
Sami <samiberakih3@gmail.com>
2024-08-17 20:55:31 +0300

    change commited content

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -3,11 +3,13 @@ import ProfilePage from './ProfilePage';
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return <ProfilePage userData={userData} />;
+  return (
+    <UserContext.Provider value={userData}>
+        <ProfilePage />
+    </UserContext.Provider>
+);
 
-<UserContext.Provider value={userData}>
-  <ProfilePage />
-</UserContext.Provider>;
+
 }
 
 export default App;

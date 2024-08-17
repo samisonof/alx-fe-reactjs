a3913c7fdde6002c8c7bf6734928d135e9719fe7
Sami <samiberakih3@gmail.com>
2024-08-17 21:07:35 +0300

    add changed contents

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -1,13 +1,15 @@
 import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return (
-    <UserContext.Provider value={userData}>
+  return <ProfilePage userData={userData} />;
+  
+  <UserContext.Provider value={userData}>
         <ProfilePage />
-    </UserContext.Provider>
-);
+    </UserContext.Provider>;
+
 
 
 }

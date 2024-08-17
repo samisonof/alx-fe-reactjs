833dec9831b0773aebdb725e8a74903af505a475
Sami <samiberakih3@gmail.com>
2024-08-17 21:58:04 +0300

    add app.jsx

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import UserContext from './UserContext';  // Import UserContext
+import UserContext from './UserContext';  // Import the UserContext
 import ProfilePage from './ProfilePage';  // Import ProfilePage
 
 function App() {
-  // Define userData that will be passed to the context
-  const userData = {
-    name: "Jane Doe",
-    email: "jane.doe@example.com"
-  };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  // Wrap ProfilePage inside UserContext.Provider and pass userData as value
   return (
     <UserContext.Provider value={userData}>
       <ProfilePage />

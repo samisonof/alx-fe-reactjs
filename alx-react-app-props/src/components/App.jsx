20056f25ccb5896101f77ab75e9cc6f02f700791
Sami <samiberakih3@gmail.com>
2024-08-17 21:27:29 +0300

    wrap content

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -5,7 +5,8 @@ import ProfilePage from './ProfilePage';
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return (
+  return <ProfilePage userData={userData} />;
+  (
     <UserContext.Provider value={userData}>
         <ProfilePage />
     </UserContext.Provider>

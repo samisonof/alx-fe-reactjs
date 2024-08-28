0d165ea4571a07aaef96a828125615797d6d99a5
Sami <samiberakih3@gmail.com>
2024-08-28 15:40:40 +0300

    change commit

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Profile from "./components/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
+import BlogPost from "./components/BlogPost";
 
 const App = () => {
   const isAuthenticated = true

cf9b6f69841d6fa987903f8212c34bdad26c6650
Sami <samiberakih3@gmail.com>
2024-08-29 18:52:55 +0300

    change commit

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -3,10 +3,11 @@ import React, {useState} from 'react';
 const TodoList = ({ todos, toggleTodo, deleteTodo }) => {
   if (todos.length === 0) {
     return <p>No todos available</p>;
+    
   }
 
   return (
-    
+
     <ul>
       {todos.map((todo) => (
         <li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>

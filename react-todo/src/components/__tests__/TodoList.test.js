d95dee7cf7a0bfc3f96e55a100bb72958497c030
Sami <samiberakih3@gmail.com>
2024-08-31 17:11:08 +0300

    commit react todo

--- react-todo/src/components/__tests__/TodoList.test.js
+++ react-todo/src/components/__tests__/TodoList.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import TodoList from '../components/TodoList';
+import TodoList from '../TodoList';
 
 describe('TodoList Component', () => {
   test('renders todo items', () => {

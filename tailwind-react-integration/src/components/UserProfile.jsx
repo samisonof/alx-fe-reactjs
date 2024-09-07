fd4b0cd4beddc19a3c924814b714f2e55d960964
Sami <samiberakih3@gmail.com>
2024-09-07 13:55:14 +0300

    commit responsive utilities

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
     return (
-      <div className="user-profile bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-        <img className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto" src="https://via.placeholder.com/150" alt="User"/>
-        <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-sm sm:text-base md:text-lg text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <div className="user-profile bg-gray-100 md:p-8 sm:p4 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+        <img className="rounded-full sm:w-24 h-24 md:w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User"/>
+        <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+        <p className="sm:text-sm md:text-base text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }

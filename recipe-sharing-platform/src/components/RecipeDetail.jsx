ee0429b64e1913c9c4769274311cd26b6daa1400
Sami <samiberakih3@gmail.com>
2024-09-10 00:12:05 +0300

    change 3'rd commit

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -19,8 +19,9 @@ const RecipeDetail = () => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
-      <img src={recipe.image} alt={recipe.title} className="w-full h-64 object-cover rounded-lg" />
+    <div className="max-w-4xl mx-auto p-4 shadow-lg rounded-lg bg-white">
+      {/* Apply shadow class to give the card-like layout some depth */}
+      <img src={recipe.image} alt={recipe.title} className="w-full h-64 object-cover rounded-lg shadow-md" />
       <h1 className="text-4xl font-bold mt-4">{recipe.title}</h1>
       <p className="text-gray-700 mt-2">{recipe.summary}</p>
 

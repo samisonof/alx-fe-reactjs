9c1d3a055abcbd7d10c8d7aa098e7f224b0abe7f
Sami <samiberakih3@gmail.com>
2024-09-10 00:15:52 +0300

    3rd commit

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import { data } from "autoprefixer";
 import React,{ useState, useEffect } from "react";
+import { Link } from 'react-router-dom'; 
 
 const HomePage = () => {
     const [recipes, setRecipes] = useState([]);

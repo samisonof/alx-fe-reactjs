dd4f9c303ac9b1de50770fdef0e637486a304d4d
Sami <samiberakih3@gmail.com>
2024-09-21 14:18:20 +0300

    3'rd commit

---
+++ github-user-search/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';  // Ensure this is here
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();

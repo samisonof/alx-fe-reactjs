2574d2b31c82a3d81e662ea4895c768a131c3c2b
Sami <samiberakih3@gmail.com>
2024-09-21 14:21:53 +0300

    second commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,18 @@
 import axios from 'axios';
 
+// Function to fetch user data based on the search term
+const fetchUserData = async (username) => {
+  const endpoint = `https://api.github.com/users/${username}`;
+
+  try {
+    const response = await axios.get(endpoint);
+    return response.data;
+  } catch (error) {
+    throw new Error('User not found');
+  }
+};
+
+// Function for advanced search with parameters like location and repo count
 const fetchAdvancedUserData = async ({ username, location, minRepos }) => {
   let query = '';
 
@@ -7,7 +20,6 @@ const fetchAdvancedUserData = async ({ username, location, minRepos }) => {
   if (location) query += `location:${location} `;
   if (minRepos) query += `repos:>${minRepos} `;
 
-  // Construct API endpoint using the query
   const endpoint = `https://api.github.com/search/users?q=${encodeURIComponent(query)}`;
 
   try {
@@ -18,4 +30,4 @@ const fetchAdvancedUserData = async ({ username, location, minRepos }) => {
   }
 };
 
-export default fetchAdvancedUserData;
+export { fetchUserData, fetchAdvancedUserData };
